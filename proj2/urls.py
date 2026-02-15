94e7930d522da2789db47983cb894d9fbe1cacd6
GSRLegend <gsimpsonrister@gmail.com>
2026-02-15 18:19:51 -0500

    Recommendations page and template functionality

--- proj2/urls.py
+++ proj2/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('recommendations/', include('recommendations.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
